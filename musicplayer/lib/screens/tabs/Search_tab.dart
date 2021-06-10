be1b1543dc71976824b9135c03aff11634a2a683
SaeedAllGray <saeedag.imail@gmail.com>
2021-06-10 12:51:55 +0430

    search page was created.

--- musicplayer/lib/screens/tabs/Search_tab.dart
+++ musicplayer/lib/screens/tabs/Search_tab.dart
@@ -12,6 +12,78 @@ class _SearchTabState extends State<SearchTab> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Container(
+          margin: EdgeInsets.all(20),
+          child: Text("Search"),
+        ),
+        centerTitle: false,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.zero,
+            margin: EdgeInsets.fromLTRB(24, 10, 24, 10),
+            height: 50,
+            // width: 312,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: TextField(
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                fillColor: Colors.black,
+                hintText: "Search",
+                hintStyle: TextStyle(color: Colors.black),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            child: Text(
+              "Your Top Genres",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Container(
+            child: Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+                children: [
+                  Container(
+                    height: 20,
+                    width: 20,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    height: 20,
+                    width: 20,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    height: 20,
+                    width: 20,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
