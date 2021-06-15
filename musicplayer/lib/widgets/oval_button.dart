f4838d076e9b63ac4ce3b4cb6a9a418f37657dd5
SaeedAllGray <saeedag.imail@gmail.com>
2021-06-15 09:35:07 +0430

    Oval button was edited.

--- musicplayer/lib/widgets/oval_button.dart
+++ musicplayer/lib/widgets/oval_button.dart
@@ -16,30 +16,33 @@ class _OvalButtonState extends State<OvalButton> {
     return ConstrainedBox(
       constraints: BoxConstraints(minWidth: 80),
       child: Container(
-          height: 28,
+          // height: 28,
           decoration: BoxDecoration(
-              // border: Border.all(
-              //   width: 2,
-              //   color: Colors.pink.shade600,
-              // ),
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(227, 0, 152, 1),
-                  Color.fromRGBO(227, 0, 152, 1),
-                  Color.fromRGBO(75, 50, 250, 1),
-                  Colors.lightBlue,
-                ],
-              ),
-              borderRadius: BorderRadius.circular(100)),
+            borderRadius: BorderRadius.circular(100),
+            // border: Border.all(
+            //   width: 2,
+            //   color: Colors.pink.shade600,
+            // ),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(227, 0, 152, 1),
+                Color.fromRGBO(227, 0, 152, 1),
+                Color.fromRGBO(75, 50, 250, 1),
+                Colors.lightBlue,
+              ],
+            ),
+          ),
           child: OutlinedButton(
-            style: ButtonStyle(),
+            style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100))),
             onPressed: () => this.widget.onPressed(),
             child: Text(
               widget.title,
               maxLines: 1,
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 25,
+                fontSize: 20,
               ),
             ),
           )),
